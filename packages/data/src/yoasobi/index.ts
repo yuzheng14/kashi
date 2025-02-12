b9ab2148452e6f1ff52710881bf82adc9256a6e5
yuzheng14 <422450117@qq.com>
2025-02-12 23:08:49 +0800

    feat(data): :sparkles: add 群青

--- packages/data/src/yoasobi/index.ts
+++ packages/data/src/yoasobi/index.ts
@@ -1,6 +1,8 @@
 import type { SongJsonSchema } from '@nl/schema/type'
 import _セブンティーン from './セブンティーン.json'
+import _群青 from './群青.json'
 
 export const yoasobi = {
   セブンティーン: _セブンティーン as SongJsonSchema,
+  群青: _群青 as SongJsonSchema,
 }
