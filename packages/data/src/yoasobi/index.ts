19197d4c4e29506508dd75e0a2c1df530060f30d
yuzheng14 <422450117@qq.com>
2025-02-13 20:01:23 +0800

    feat(data): :sparkles: add 祝福

--- packages/data/src/yoasobi/index.ts
+++ packages/data/src/yoasobi/index.ts
@@ -3,10 +3,12 @@ import _セブンティーン from './セブンティーン.json'
 import _群青 from './群青.json'
 import _夜に駆ける from './夜に駆ける.json'
 import _アイドル from './アイドル.json'
+import _祝福 from './祝福.json'
 
 export const yoasobi = {
   セブンティーン: _セブンティーン as SongJsonSchema,
   群青: _群青 as SongJsonSchema,
   夜に駆ける: _夜に駆ける as SongJsonSchema,
   アイドル: _アイドル as SongJsonSchema,
+  祝福: _祝福 as SongJsonSchema,
 }
