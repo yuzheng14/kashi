6e5898fa52a3dfaff23b319d665ac2599fbafe5b
yuzheng14 <422450117@qq.com>
2025-02-13 21:23:25 +0800

    feat(data): :sparkles: add UNDEAD

--- packages/data/src/yoasobi/index.ts
+++ packages/data/src/yoasobi/index.ts
@@ -4,6 +4,7 @@ import _群青 from './群青.json'
 import _夜に駆ける from './夜に駆ける.json'
 import _アイドル from './アイドル.json'
 import _祝福 from './祝福.json'
+import _UNDEAD from './UNDEAD.json'
 
 export const yoasobi = {
   セブンティーン: _セブンティーン as SongJsonSchema,
@@ -11,4 +12,5 @@ export const yoasobi = {
   夜に駆ける: _夜に駆ける as SongJsonSchema,
   アイドル: _アイドル as SongJsonSchema,
   祝福: _祝福 as SongJsonSchema,
+  UNDEAD: _UNDEAD as SongJsonSchema,
 }
