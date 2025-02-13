e82e0563027ef5fac33ef8f4cf426961f93def05
yuzheng14 <422450117@qq.com>
2025-02-13 19:12:04 +0800

    feat(data): :sparkles: add 夜に駆ける

--- packages/data/src/yoasobi/index.ts
+++ packages/data/src/yoasobi/index.ts
@@ -1,8 +1,10 @@
 import type { SongJsonSchema } from '@kashijs/schema/type'
 import _セブンティーン from './セブンティーン.json'
 import _群青 from './群青.json'
+import _夜に駆ける from './夜に駆ける.json'
 
 export const yoasobi = {
   セブンティーン: _セブンティーン as SongJsonSchema,
   群青: _群青 as SongJsonSchema,
+  夜に駆ける: _夜に駆ける as SongJsonSchema,
 }
