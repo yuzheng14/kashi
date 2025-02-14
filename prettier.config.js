7e0e56a8fa52a6d61285abf494083cc07b927d2b
yuzheng14 <422450117@qq.com>
2025-02-14 14:01:20 +0800

    style(data): :art: modify config of prettier and format

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,10 @@ export default {
   semi: false,
   singleQuote: true,
   htmlWhitespaceSensitivity: 'ignore',
-};
+  overrides: [
+    {
+      files: ['packages/data/src/common.json'],
+      options: { printWidth: 50 },
+    },
+  ],
+}
