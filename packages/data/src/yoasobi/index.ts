7a518c59cdb43335ccc92e7fa03ebced45aac62a
yuzheng14 <422450117@qq.com>
2025-02-14 12:53:32 +0800

    feat(data): :sparkles: add template, だぶん, 勇者, 好きだ

--- packages/data/src/yoasobi/index.ts
+++ packages/data/src/yoasobi/index.ts
@@ -6,6 +6,9 @@ import _アイドル from './アイドル.json'
 import _祝福 from './祝福.json'
 import _UNDEAD from './UNDEAD.json'
 import _HEART_BEAT from './HEART BEAT.json'
+import _好きだ from './好きだ.json'
+import _だぶん from './だぶん.json'
+import _勇者 from './勇者.json'
 
 export const yoasobi = {
   セブンティーン: _セブンティーン as SongJsonSchema,
@@ -15,4 +18,7 @@ export const yoasobi = {
   祝福: _祝福 as SongJsonSchema,
   UNDEAD: _UNDEAD as SongJsonSchema,
   'HEART BEAT': _HEART_BEAT as SongJsonSchema,
+  好きだ: _好きだ as SongJsonSchema,
+  だぶん: _だぶん as SongJsonSchema,
+  勇者: _勇者 as SongJsonSchema,
 }
